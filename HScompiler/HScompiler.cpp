9adcb2250f89f5988a288fffbaab6028b8877706
TheScaryParrot <fabio.stalder@gmx.ch>
2024-01-31 13:18:14 -0500

    Started adding more command arguments (eg. full compile to exe and output file name)

--- HScompiler/HScompiler.cpp
+++ HScompiler/HScompiler.cpp
@@ -59,6 +59,59 @@ void CompileFile(std::ifstream* inputFile, std::ofstream* outputFile)
     }
 }
 
+/// @brief Exectures the compiler
+/// @param inputFilePath Path to the file to compile
+/// @param outputFilePath Path to the file to output to
+/// @param compileToExe Whether to compile all the way to an executable
+void Execute(std::string inputFilePath, std::string outputFilePath, bool compileToExe)
+{
+    // INPUT FILE
+    if (inputFilePath == "")
+    {
+        std::cout << "inputFilePath is null\n";
+        return;
+    }
+
+    std::ifstream inputFile(inputFilePath);
+
+    if (!inputFile.is_open())
+    {
+        std::cout << "Could not open input file\n";
+        return;
+    }
+
+
+    // OUTPUT FILE
+    if (outputFilePath == "")
+    {
+        outputFilePath = "output.s";
+    }
+
+    std::ofstream outputFile(outputFilePath);
+
+    if (!outputFile.is_open())
+    {
+        std::cout << "Could not open output file\n";
+        return;
+    }
+
+    // COMPILATION
+    CompileFile(&inputFile, &outputFile);
+
+    /*if (compileToExe)
+    {
+        std::string objectFilePath = outputFilePath + ".o";
+        system(std::string("nasm -f elf64 " + assemblyFilePath + " -o " + objectFilePath).c_str()); //command = "nasm -f elf64 OUTFILE -o OUTFILE.o"
+
+        std::string exeFilePath = outputFilePath;
+        system(std::string("ld " + objectFilePath + " -o " + outputFilePath).c_str()); //command = "ld OUTFILE.o -o OUTFILE"
+
+        // Remove the object file and assembly file
+        //system(std::string("rm " + objectFilePath).c_str()); //command = "rm OUTFILE.o"
+        //system(std::string("rm " + assemblyFilePath).c_str()); //command = "rm OUTFILE.s"
+    }*/
+}
+
 int main(int argc, char* argv[])
 {
     std::cout << "Hello from the HScompiler\n";
@@ -68,27 +121,44 @@ int main(int argc, char* argv[])
         std::cout << "No input file specified\n";
         return 1;
     }
-    if (argc > 2)
+
+    std::string inputFile = argv[1];
+
+    int currentArgIndex = 2;
+    std::string outputFile = "";
+    bool compileToExe = false;
+
+    while (currentArgIndex < argc)
     {
-        std::cout << "More than one argument was specified\nOnly the first argument will be used\n";
+        std::string arg = argv[currentArgIndex];
+
+        if (arg == "-o")
+        {
+            // If this is the last argument
+            if (argc < currentArgIndex + 2)
+            {
+                std::cout << "No output file specified\n";
+                return 1;
+            }
+
+            outputFile = argv[currentArgIndex + 1];
+
+            currentArgIndex += 2;
+        }
+        else if (arg == "-c")
+        {
+            compileToExe = true;
+            currentArgIndex++;
+        }
+        
+        else
+        {
+            std::cout << "Unknown argument: " << arg << "\n";
+            return 1;
+        }
     }
     
-    std::ifstream inputFile(argv[1]);
-    std::ofstream outputFile("output.s");
-
-    if (!inputFile.is_open())
-    {
-        std::cout << "Could not open input file\n";
-        return 1;
-    }
-
-    if (!outputFile.is_open())
-    {
-        std::cout << "Could not open output file\n";
-        return 1;
-    }
-
-    CompileFile(&inputFile, &outputFile);
+    Execute(inputFile, outputFile, compileToExe);
 
     return 0;
 }
