2a61ddcb1e22907cf8ec90f42c9a369c074f4c9b
TheScaryParrot <fabio.stalder@gmx.ch>
2024-05-23 17:52:01 +0200

    fix: Support for properties of a type of a type from an environment

--- THSCompiler/library/codeGenerator/environment/Environment.cpp
+++ THSCompiler/library/codeGenerator/environment/Environment.cpp
@@ -15,31 +15,24 @@ public:
     void AddVariable(std::string identifier, std::shared_ptr<Variable> variable);
     /// @brief Returns a variable that is defined in the current environment
     std::shared_ptr<Variable> GetVariable(std::string identifier);
-    /// @brief Returns a variable that is defined in the given type identifier
-    std::shared_ptr<Variable> GetVariable(std::string typeIdentifier, std::string identifier);
-    /// @brief Returns a variable that is defined in the given type
-    std::shared_ptr<Variable> GetVariable(std::shared_ptr<Type> type, std::string identifier);
 
 
     /// @brief Adds the given function to the current environment with the given identifier
     void AddFunction(std::string identifier, std::shared_ptr<Function> function);
     /// @brief Returns a function that is defined in the current environment
     std::shared_ptr<Function> GetFunction(std::string identifier);
-    /// @brief Returns a function that is defined in the given type identifier
-    std::shared_ptr<Function> GetFunction(std::string typeIdentifier, std::string identifier);
-    /// @brief Returns a function that is defined in the given type
-    std::shared_ptr<Function> GetFunction(std::shared_ptr<Type> type, std::string identifier);
 
 
     /// @brief Adds the given type to the current environment with the given identifier
     void AddType(std::string identifier, std::shared_ptr<Type> type, std::shared_ptr<Environment> environment);
     /// @brief Returns a type that is defined in the current environment
     std::shared_ptr<Type> GetType(std::string identifier);
-    /// @brief Returns a type that is defined in the given type identifier
-    std::shared_ptr<Type> GetType(std::string typeIdentifier, std::string identifier);
-    /// @brief Returns a type that is defined in the given type
-    std::shared_ptr<Type> GetType(std::shared_ptr<Type> type, std::string identifier);
 
+
+    /// @brief Returns the environment of the given type identifier
+    std::shared_ptr<Environment> GetEnvironment(std::string typeIdentifier);
+    /// @brief Returns the environment of the given type
+    std::shared_ptr<Environment> GetEnvironment(std::shared_ptr<Type> type);
 
 private:
     typedef std::pair<std::shared_ptr<Type>, std::shared_ptr<Environment>> TypeEnvironment;
@@ -72,16 +65,6 @@ std::shared_ptr<Variable> Environment::GetVariable(std::string identifier)
     return variables.GetElement(identifier);
 }
 
-std::shared_ptr<Variable> Environment::GetVariable(std::string typeIdentifier, std::string identifier)
-{
-    return GetEnvironment(GetTypeEnvironment(typeIdentifier))->GetVariable(identifier);
-}
-
-std::shared_ptr<Variable> Environment::GetVariable(std::shared_ptr<Type> type, std::string identifier)
-{
-    return GetEnvironment(GetTypeEnvironment(type))->GetVariable(identifier);
-}
-
 
 void Environment::AddFunction(std::string identifier, std::shared_ptr<Function> function)
 {
@@ -91,16 +74,6 @@ void Environment::AddFunction(std::string identifier, std::shared_ptr<Function> 
 std::shared_ptr<Function> Environment::GetFunction(std::string identifier)
 {
     return functions.GetElement(identifier);
-}
-
-std::shared_ptr<Function> Environment::GetFunction(std::string typeIdentifier, std::string identifier)
-{
-    return GetEnvironment(GetTypeEnvironment(typeIdentifier))->GetFunction(identifier);
-}
-
-std::shared_ptr<Function> Environment::GetFunction(std::shared_ptr<Type> type, std::string identifier)
-{
-    return GetEnvironment(GetTypeEnvironment(type))->GetFunction(identifier);
 }
 
 
@@ -114,14 +87,15 @@ std::shared_ptr<Type> Environment::GetType(std::string identifier)
     return GetType(GetTypeEnvironment(identifier));
 }
 
-std::shared_ptr<Type> Environment::GetType(std::string typeIdentifier, std::string identifier)
+
+std::shared_ptr<Environment> Environment::GetEnvironment(std::string typeIdentifier)
 {
-    return GetEnvironment(GetTypeEnvironment(typeIdentifier))->GetType(identifier);
+    return GetEnvironment(GetTypeEnvironment(typeIdentifier));
 }
 
-std::shared_ptr<Type> Environment::GetType(std::shared_ptr<Type> type, std::string identifier)
+std::shared_ptr<Environment> Environment::GetEnvironment(std::shared_ptr<Type> type)
 {
-    return GetEnvironment(GetTypeEnvironment(type))->GetType(identifier);
+    return GetEnvironment(GetTypeEnvironment(type));
 }
 
 
