8196c204248a9e0341de51d4cb3cfb00ef165841
TheScaryParrot <fabio.stalder@gmx.ch>
2024-05-27 17:46:59 +0200

    feat: Added AssemblyLabelLine.cpp

---
+++ THSCompiler/library/assembly/AssemblyLabelLine.cpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "IAssemblyLine.cpp"
+
+class AssemblyLabelLine : public IAssemblyLine
+{
+public:
+    AssemblyLabelLine(std::string label);
+
+    std::string ToString() override;
+
+private:
+    std::string label;
+};
+
+AssemblyLabelLine::AssemblyLabelLine(std::string label)
+{
+    this->label = label;
+}
