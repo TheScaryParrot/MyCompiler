66cc05ce6157f02f951aa8a21c502ba8d7581d8c
TheScaryParrot <fabio.stalder@gmx.ch>
2024-07-18 11:53:47 +0200

    feat(THS): Cleared old codeGenerator code from before just C rework

--- THSCompiler/library/codeGenerator/CodeGenerator.cpp
+++ THSCompiler/library/codeGenerator/CodeGenerator.cpp
@@ -10,39 +10,4 @@
 class CodeGenerator
 {
    public:
-    enum class CodeGeneratorStates
-    {
-        GLOBAL,
-        FUNCTION,
-        CLASS
-    } state;
-
-    IVariableLocation* relAccessBaseVarLocation;
-    EnvironmentLinkedList* environmentList;
-
-    const std::string CLASS_SIZE_COMPILE_VAR = "classSize";
-
-    CodeGenerator()
-    {
-        state = CodeGeneratorStates::GLOBAL;
-        relAccessBaseVarLocation = nullptr;
-        environmentList = new EnvironmentLinkedList();
-        environmentList->Push(new Environment());
-    }
-
-    IVariableLocation* AllocateNewVariableLocation(Type* type, AssemblyCode* assemblyCode)
-    {
-        // TODO: Look for registries, allocate on stack, stuff like that
-        AssemblyInstructionLine* line = new AssemblyInstructionLine(AssemblyInstructions::ADD);
-        line->AddArgument("esp");
-        line->AddArgument("TEST");
-        assemblyCode->AddLine(line);
-
-        return new RegistryVarLocation("eax", 0, type);
-    }
-
-    void GenerateCodeAssign(IVariableLocation* dest, IVariableLocation* src, AssemblyCode* assemblyCode)
-    {
-        // TODO: Call assign function
-    }
 };
