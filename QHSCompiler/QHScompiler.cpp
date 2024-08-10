35d743849d947073b08f5a0b884989d186502461
TheScaryParrot <fabio.stalder@gmx.ch>
2024-08-10 18:11:35 +0200

    feat(QHS): Added argparse (pretty much copied over from THScompiler)

--- QHSCompiler/QHScompiler.cpp
+++ QHSCompiler/QHScompiler.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
 #include "library/InputFile.cpp"
+#include "library/OutputFile.cpp"
 #include "library/codeGenerator/Generator.cpp"
+#include "library/utils/argparse.hpp"
 
-void CompileFile(std::string filename)
+AssemblyCode* CompileFile(std::string filename)
 {
     InputFile* file = new InputFile(filename);
 
@@ -13,13 +15,94 @@ void CompileFile(std::string filename)
 
     delete file;
 
-    std::cout << assemblyCode->ToString() << "\n";
+    return assemblyCode;
+}
 
-    delete assemblyCode;
+void OutToFile(AssemblyCode* code, std::string filename)
+{
+    OutputFile outFile = OutputFile(filename);
+    outFile.Write(code->ToString());
+}
+
+void Assemble(std::string inFile, std::string outFile, bool deleteInFile)
+{
+    Logger.Log("Assembling " + inFile + " to " + outFile, Logger::INFO);
+
+    std::string command = "nasm -felf64 " + inFile + " -o " + outFile;
+    system(command.c_str());
+
+    if (deleteInFile)
+    {
+        std::string command = "rm " + inFile;
+        system(command.c_str());
+    }
+}
+
+void Link(std::string inFile, std::string outFile, bool deleteInFile)
+{
+    Logger.Log("Linking " + inFile + " to " + outFile, Logger::INFO);
+
+    std::string command = "ld " + inFile + " -o " + outFile;
+    system(command.c_str());
+
+    if (deleteInFile)
+    {
+        std::string command = "rm " + inFile;
+        system(command.c_str());
+    }
 }
 
 int main(int argc, char const* argv[])
 {
-    CompileFile(argv[1]);
+    argparse::ArgumentParser program("THS compiler");
+
+    program.add_argument("file").help("The file to compile");
+    program.add_argument("-o", "--output")
+        .help("The output file. If none is specified the output will be stored in a.out")
+        .default_value("a.out");
+
+    program.add_argument("-c", "--compile")
+        .help("Only compile, do not assemble and link")
+        .default_value(false)
+        .implicit_value(true);
+    program.add_argument("-s", "--assemble")
+        .help("Only compile and assemble, do not link")
+        .default_value(false)
+        .implicit_value(true);
+    program.add_argument("-k", "--keep").help("Keep temporary files").default_value(false).implicit_value(true);
+
+    try
+    {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error& err)
+    {
+        std::cout << err.what() << std::endl;
+        std::cout << program;
+        exit(0);
+    }
+
+    std::string filePath = program.get<std::string>("file");
+    AssemblyCode* outCode = CompileFile(filePath);
+    bool deleteTempFiles = !program.get<bool>("--keep");
+
+    if (program["--compile"] == true)
+    {
+        OutToFile(outCode, program.get<std::string>("--output"));
+    }
+    else if (program["--assemble"] == true)
+    {
+        OutToFile(outCode, "a.s");
+        Assemble("a.s", program.get<std::string>("--output"), deleteTempFiles);
+    }
+    else
+    {
+        OutToFile(outCode, "a.s");
+        Assemble("a.s", "a.o", deleteTempFiles);
+        Link("a.o", program.get<std::string>("--output"), deleteTempFiles);
+    }
+
+    delete outCode;
+
     return 0;
 }
