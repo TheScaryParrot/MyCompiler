103f3403aee32cd533bba5aa7c92318c9d27f858
TheScaryParrot <fabio.stalder@gmx.ch>
2024-08-30 20:33:59 +0200

    feat(QHS): Optimized OrderQueue datastructure to not use vectors

--- QHSCompiler/library/codeGenerator/OrderQueue.cpp
+++ QHSCompiler/library/codeGenerator/OrderQueue.cpp
@@ -4,24 +4,69 @@
 
 #include "Order.cpp"
 
+struct Node
+{
+    Order order = Order::Empty();
+    Node* next;
+    Node* prev;
+};
+
 class OrderQueue
 {
    public:
     OrderQueue() = default;
     OrderQueue(Order order) { this->Enqueue(order); }
 
-    void Enqueue(Order order) { this->queue.push_back(order); }
-    void EnqueueInFront(Order order) { queue.insert(queue.begin(), order); }
+    void Enqueue(Order order)
+    {
+        Node* newNode = new Node();
+        newNode->order = order;
+        newNode->next = nullptr;
+        newNode->prev = nullptr;
+
+        if (head == nullptr)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+    void EnqueueInFront(Order order)
+    {
+        Node* newNode = new Node();
+        newNode->order = order;
+        newNode->next = nullptr;
+        newNode->prev = nullptr;
+
+        if (head == nullptr)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            head->prev = newNode;
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
     Order Dequeue()
     {
         if (IsEmpty()) return Order::Empty();
 
-        Order order = this->queue.front();
-        this->queue.erase(queue.begin());
-        return order;
+        Node* current = head;
+        head = head->next;
+        if (head != nullptr) head->prev = nullptr;
+        return current->order;
     }
-    Order Front() { return this->queue.front(); }
-    bool IsEmpty() { return this->queue.empty(); }
+    Order Front() { return this->head->order; }
+    bool IsEmpty() { return this->head == nullptr; }
     void Clear()
     {
         while (!IsEmpty()) Dequeue();
@@ -31,14 +76,18 @@ class OrderQueue
     {
         std::string str = "";
 
-        for (Order order : queue)
+        Node* current = head;
+
+        while (current != nullptr)
         {
-            str += order.ToString() + "\n";
+            str += current->order.ToString() + "\n";
+            current = current->next;
         }
 
         return str;
     }
 
    private:
-    std::vector<Order> queue;
+    Node* head = nullptr;
+    Node* tail = nullptr;
 };
