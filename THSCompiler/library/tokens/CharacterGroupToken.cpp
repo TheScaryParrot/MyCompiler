5a83dbf2c594969d4e02b2bb94f0963332bb92e6
TheScaryParrot <fabio.stalder@gmx.ch>
2024-09-26 21:10:37 +0200

    feat(THS): Implemented TODO for more efficient way of storing CharacterGroups

--- THSCompiler/library/tokens/CharacterGroupToken.cpp
+++ THSCompiler/library/tokens/CharacterGroupToken.cpp
@@ -5,16 +5,15 @@
 class CharacterGroupToken : public Token
 {
    public:
-    CharacterGroupToken(std::string tokenName, unsigned int line, CharacterGroup& characterGroup) : Token(tokenName, line)
+    CharacterGroupToken(std::string tokenName, unsigned int line, CharacterGroup* characterGroup) : Token(tokenName, line)
     {
         this->characterGroup = characterGroup;
     }
 
-    unsigned int Match(char first, char second) { return characterGroup.Match(first, second); }
+    unsigned int Match(char first, char second) { return characterGroup->Match(first, second); }
 
     CharacterGroupToken* New(unsigned int line) { return new CharacterGroupToken(this->tokenName, line, this->characterGroup); }
 
    private:
-    // TODO: Use more efficient way to store characterGroup (pointer or reference)
-    CharacterGroup characterGroup;
+    CharacterGroup* characterGroup;
 };
